83c62bc0868d2bcf4c5d810e253745cb8de81185
Syafiqjos <syafiqjos@gmail.com>
2021-11-04 00:36:44 +0700

    Add CopyrightFooter component

---
+++ src/components/CopyrightFooter.js
@@ -0,0 +1,11 @@
+import './CopyrightFooter.css';
+
+function CopyrightFooter() {
+    return (
+        <div className="footer">
+            <span>Copyright Ahmad Syafiq Aqil Wafi 2021. All rights reserved.</span>
+        </div>
+    );
+}
+
+export default CopyrightFooter;
