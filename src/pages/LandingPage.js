26623761736e6e406c3cb2e9728e6b144bf225c6
Syafiqjos <syafiqjos@gmail.com>
2021-11-04 00:37:06 +0700

    Update LandingPage structure and add CopyrightFooter component

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,10 +2,17 @@ import './LandingPage.css';
 
 // Components
 import Header from '../components/Header';
+import CopyrightFooter from '../components/CopyrightFooter';
 
 function LandingPage() {
     return (
-        <Header />
+        <div className="page">
+            <Header />
+            <main>
+                
+            </main>
+            <CopyrightFooter />
+        </div>
     );
 }
 
