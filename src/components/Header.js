0d0da3389d92ccd600a46ab49740a88bfe1a2d64
Syafiqjos <syafiqjos@gmail.com>
2021-11-04 00:15:56 +0700

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import './Header.css';
+import HomeIcon from '../icons/home.svg';
+
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <header className="header">
+            <a href="/">
+                <div className="header__logo">
+                    <img className="header__logo-img" src={ HomeIcon } alt="logo" />
+                    <span className="header__logo-text">LinkShortener.me</span>
+                </div>
+            </a>
+            <nav className="navigation">
+                <ul className="navigation__links">
+                    <li className="navigation__link-item">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="navigation__link-item">
+                        <Link to="/about-us">About Us</Link>
+                    </li>
+                    <li className="navigation__link-item">
+                        <Link to="/tos">ToS</Link>
+                    </li>
+                    <li className="navigation__link-item">
+                        <Link className="btn btn--secondary" href="/sign-in">Sign In</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
